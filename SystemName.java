9ee32ce66f890ca25480440f32c294650a485a74
natashamccabe <87022961+natashamccabe@users.noreply.github.com>
2022-11-21 23:21:58 +0000

    Update SystemName.java

--- SystemName.java
+++ SystemName.java
@@ -3,10 +3,6 @@
  */
 package artemisLite;
 
-/**
- * @author emmadeane
- *
- */
 public enum SystemName {
 
 	GOVERNMENT_GRANT, SPACE_LAUNCH_SYSTEM, ORION, GATEWAY, ARTEMIS_GENERATION_SPACESUITS, EXTERNAL_INVESTIGATION
